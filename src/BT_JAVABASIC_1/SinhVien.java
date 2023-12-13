a0b0ca03948a350570c828eacbf6636969150b88
HienLV97 <levinhhien2603@gmail.com>
2023-12-13 19:52:10 +0700

    a

--- src/BT_JAVABASIC_1/SinhVien.java
+++ src/BT_JAVABASIC_1/SinhVien.java
@@ -1,2 +1,11 @@
-package BT_JAVABASIC_1;public class SinhVien {
+package BT_JAVABASIC_1;
+
+public class SinhVien {
+	String name = "LV Hien";
+	public int age;
+
+	public static void main(String[] args) {
+		SinhVien sv = new SinhVien();
+		System.out.println(sv.name);
+	}
 }
